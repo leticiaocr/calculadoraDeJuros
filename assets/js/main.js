d7c6ba9a2e7650aa2c913967c3b969ceb66c8fb9
leticiaocr <leticiabritooliveirac@gmail.com>
2023-05-31 16:19:38 -0300

    feature: botao limpar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,37 @@
+
+
+
+// CALCULAR O VALOR FINAL 
 document.querySelector('#calculate').addEventListener('click', function (e) {
 
     e.preventDefault();
-    
-    const value = document.querySelector('#value').value;
-    const fee = (document.querySelector('#fee').value) / 100;
-    const period = document.querySelector('#period').value;
 
-    const total = value * (1 + fee) ** period;
+    const value = parseFloat(document.querySelector('#value').value);
+    const fee = parseFloat(document.querySelector('#fee').value) / 100;
+    const period = parseFloat(document.querySelector('#period').value);
+    const monthlyValue = parseFloat(document.querySelector('#monthlyValue').value)
+
+
+    const total = (value * (1 + fee) ** period) + (monthlyValue * (((1 + fee) ** period) - 1) / fee);
+
 
     document.querySelector('#total').innerHTML = ("R$" + total.toFixed(2));
 })
+
+
+// LIMPAR INPUTS
+const clean = document.querySelector('#clean');
+const inputs = document.querySelectorAll('input');
+
+clean.addEventListener('click', function (e) {
+    e.preventDefault();
+    inputs.forEach((input) => {
+        input.value = "";
+    });
+});
+
+
+
+
+
+
