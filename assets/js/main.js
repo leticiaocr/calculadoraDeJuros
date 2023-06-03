fda35daa95094ecd3ccc83b0b867426f08908772
leticiaocr <leticiabritooliveirac@gmail.com>
2023-06-03 10:56:01 -0300

    feature: adicionando máscaras de input

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,17 @@
-
-
+// REMOVER ITENS NÃO NUMÉRICOS 
+function convertToNumber(str) {
+    return parseFloat(str.replace(/[^0-9,]+/g, '').replace(',', '.'));
+  }
 
 // CALCULAR O VALOR FINAL 
 document.querySelector('#calculate').addEventListener('click', function (e) {
 
     e.preventDefault();
 
-    const value = parseFloat(document.querySelector('#value').value);
-    const fee = parseFloat(document.querySelector('#fee').value) / 100;
-    const period = parseFloat(document.querySelector('#period').value);
-    const monthlyValue = parseFloat(document.querySelector('#monthlyValue').value)
+    const value = convertToNumber(document.querySelector('#value').value);
+    const fee = (convertToNumber(document.querySelector('#fee').value)) / 100;
+    const period = convertToNumber(document.querySelector('#period').value);
+    const monthlyValue = convertToNumber(document.querySelector('#monthlyValue').value);
 
 
     const total = (value * (1 + fee) ** period) + (monthlyValue * (((1 + fee) ** period) - 1) / fee);
